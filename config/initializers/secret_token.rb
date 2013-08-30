024974785c0050a4a98180362b81b5898e38a10b
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-08-30 14:17:31 -0400

    initial commit with bootstrap stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hike::Application.config.secret_key_base = 'cb768888eaeb7150b2536e941d87d12218b01d1853c773de3cbe4a0de34a24f88905573dea1e18965ef3cc0e75a2da93c9fb284091d5c4237c23683ad1fba4cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hike::Application.config.session_store :cookie_store, key: '_hike_session'
